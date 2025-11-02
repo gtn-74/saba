635d5bd06b953a03c22257cdd1168a0e106a2d4d
gtn-74 <gtn.r74@gmail.com>
2025-11-02 09:28:11 +0900

    clean:fmt

--- saba_core/src/lib.rs
+++ saba_core/src/lib.rs
@@ -1,4 +1,4 @@
 #![no_std]
-extern crete alloc;
+extern crate alloc;
 
 pub mod url;
--- src/main.rs
+++ src/main.rs
@@ -3,15 +3,14 @@
 // }
 
 #![no_std]
- #![cfg_attr(not(target_os = "linux"), no_main)]
+#![cfg_attr(not(target_os = "linux"), no_main)]
 
- use noli::prelude::*;
+use noli::prelude::*;
 
- fn main() {
-     Api::write_string("Hello World\n");
-     println!("Hello from println!");
-     Api::exit(42);
- }
+fn main() {
+    Api::write_string("Hello World\n");
+    println!("Hello from println!");
+    Api::exit(42);
+}
 // noli  crateのentry_point!が、main関数を起動
- entry_point!(main);
-
+entry_point!(main);
